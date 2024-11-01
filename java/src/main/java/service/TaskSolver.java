2bac1c45869421c458041a53c89c6ffae5a880c9
Kane Mullett <kane.mullett@savient.uk.com>
2024-11-01 17:39:30 +0000

    Rename

--- java/src/main/java/service/TaskSolver.java
+++ java/src/main/java/service/TaskSolver.java
@@ -6,15 +6,15 @@ import org.slf4j.LoggerFactory;
 public class TaskSolver {
     private static final Logger LOGGER = LoggerFactory.getLogger(TaskSolver.class);
 
-    private final DaySolver taskSolutions;
+    private final DaySolver daySolver;
 
-    public TaskSolver(DaySolver taskSolutions) {
-        this.taskSolutions = taskSolutions;
+    public TaskSolver(DaySolver daySolver) {
+        this.daySolver = daySolver;
     }
 
     @SuppressWarnings("java:S2629") // Not great practice to spam logs, but I'm not building API for this.
     public void solveTasks() {
-        LOGGER.info(String.format("Task 1 Solution: %s", taskSolutions.taskOne()));
-        LOGGER.info(String.format("Task 2 Solution: %s", taskSolutions.taskTwo()));
+        LOGGER.info(String.format("Task 1 Solution: %s", daySolver.taskOne()));
+        LOGGER.info(String.format("Task 2 Solution: %s", daySolver.taskTwo()));
     }
 }

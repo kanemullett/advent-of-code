7f91f13fc1f9bc7ca86846bf25efa4165137878d
Kane Mullett <kane.mullett@savient.uk.com>
2024-11-03 21:49:51 +0000

    Input

--- java/src/main/java/challenges/aoc2023/day2/service/DayTwoSolver.java
+++ java/src/main/java/challenges/aoc2023/day2/service/DayTwoSolver.java
@@ -1,8 +1,12 @@
 package challenges.aoc2023.day2.service;
 
 import service.DaySolver;
+import util.AdventUtils;
+
+import java.util.List;
 
 public class DayTwoSolver implements DaySolver {
+    private final List<String> inputLines = AdventUtils.parseInput(2023, 2);
 
     public String taskOne() {
         return null;

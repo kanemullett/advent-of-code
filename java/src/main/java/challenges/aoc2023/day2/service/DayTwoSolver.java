c984289bb8c7751496edfb64aa456c006747e0cd
Kane Mullett <kane.mullett@savient.uk.com>
2024-11-03 21:44:12 +0000

    Day two solver

---
+++ java/src/main/java/challenges/aoc2023/day2/service/DayTwoSolver.java
@@ -0,0 +1,14 @@
+package challenges.aoc2023.day2.service;
+
+import service.DaySolver;
+
+public class DayTwoSolver implements DaySolver {
+
+    public String taskOne() {
+        return null;
+    }
+
+    public String taskTwo() {
+        return null;
+    }
+}

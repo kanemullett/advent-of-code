bc84ab504806d3447cf3a1962786b692bbb260e6
Kane Mullett <kane.mullett@savient.uk.com>
2024-12-02 13:40:14 +0000

    Javadoc

--- java/src/main/java/challenges/aoc2024/day2/model/ReactorReport.java
+++ java/src/main/java/challenges/aoc2024/day2/model/ReactorReport.java
@@ -4,11 +4,26 @@ import java.util.List;
 
 import org.immutables.value.Value.Immutable;
 
+/**
+ * ReactorReport object containing the reactor's reported levels.
+ */
 @Immutable
 public interface ReactorReport {
     
+    /**
+     * The reactor's reported levels.
+     * 
+     * @return List of reported levels.
+     */
     List<Integer> getLevels();
 
+    /**
+     * Build a ReactorReport object from the given report line.
+     * 
+     * @param reportLine The raw line of the report.
+     * 
+     * @return ReactorReport object.
+     */
     static ReactorReport of(String reportLine) {
         
         return ImmutableReactorReport.builder()

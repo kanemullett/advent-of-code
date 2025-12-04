42039c4a74aa4e9b6510b022a48c853f2ac5dc50
Kane Mullett <103215746+kanemullett@users.noreply.github.com>
2025-12-04 12:01:55 +0000

    Update TaskSolver.scala

--- scala/src/main/scala/service/TaskSolver.scala
+++ scala/src/main/scala/service/TaskSolver.scala
@@ -1,9 +1,6 @@
 package service
 
-import org.slf4j.LoggerFactory
-
 class TaskSolver(daySolver: DaySolver) {
-  private val logger = LoggerFactory.getLogger(getClass)
 
   def solveTasks(): Unit = {
     println(s"Task 1 Solution: ${daySolver.taskOne()}")
